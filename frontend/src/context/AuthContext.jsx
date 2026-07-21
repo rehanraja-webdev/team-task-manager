f57ec9220b50d2c8199dfc767e166db028362422
Rehan Raja <rehanraja.dev@gmail.com>
2026-07-21 21:00:07 +0530

    Add AuthContext and useAuth hook for user authentication management

---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,19 @@
+import { createContext, useState } from "react";
+
+export const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const value = {
+    user,
+    setUser,
+    loading,
+    setLoading,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
+
+export default AuthProvider;

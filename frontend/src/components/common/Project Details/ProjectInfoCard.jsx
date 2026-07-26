a2bac6044e509fdbb20d2e7d7135aa97d40c00f9
Rehan Raja <rehanraja.dev@gmail.com>
2026-07-27 03:07:49 +0530

    Move delete funstion to ProjectInfoCard with confirmation for deletion

--- frontend/src/components/common/Project Details/ProjectInfoCard.jsx
+++ frontend/src/components/common/Project Details/ProjectInfoCard.jsx
@@ -1,7 +1,29 @@
-import { FolderKanban, CalendarDays, UserRound } from "lucide-react";
+import {
+  FolderKanban,
+  CalendarDays,
+  UserRound,
+  Trash2,
+  Pencil,
+} from "lucide-react";
 import formatDate from "../../../utils/formatDate";
+import useProjects from "../../../hooks/useProjects";
+import { useNavigate } from "react-router";
+import LoadingSpinner from "../LoadingSpinner";
 
 const ProjectInfoCard = ({ project }) => {
+  const { deleteProject, reloadProjects, loading } = useProjects();
+  const navigate = useNavigate();
+
+  const handleDelete = async () => {
+    const confirmed = confirm("Do you want to delete this project?");
+    if (!confirmed) return;
+
+    await deleteProject(project._id);
+    navigate(-1);
+    await reloadProjects();
+  };
+
+  if (loading) return <LoadingSpinner />;
   return (
     <div className="bg-slate-900 border border-slate-800 rounded-3xl p-8 lg:col-span-2 shadow-lg">
       {/* Header */}
@@ -13,6 +35,27 @@ const ProjectInfoCard = ({ project }) => {
         <div>
           <h2 className="text-2xl font-bold text-white">Project Information</h2>
           <p className="text-slate-400 text-sm">Overview of this project</p>
+        </div>
+
+        <div className="flex items-center gap-3 shrink-0 pt-1 ml-auto">
+          <button
+            type="button"
+            aria-label="Edit task"
+            className="inline-flex items-center gap-2 px-4 py-2 text-sm font-medium text-slate-200 bg-slate-800/80 hover:bg-slate-700/80 border border-slate-700/60 rounded-lg transition-all duration-200 active:scale-95 shadow-sm"
+          >
+            <Pencil className="w-4 h-4 text-slate-400" />
+            <span>Edit</span>
+          </button>
+
+          <button
+            onClick={handleDelete}
+            type="button"
+            aria-label="Delete task"
+            className="inline-flex items-center gap-2 px-4 py-2 text-sm font-medium text-red-400 bg-red-500/10 hover:bg-red-500/20 border border-red-500/20 hover:border-red-500/30 rounded-lg transition-all duration-200 active:scale-95 shadow-sm"
+          >
+            <Trash2 className="w-4 h-4" />
+            <span>Delete</span>
+          </button>
         </div>
       </div>
 

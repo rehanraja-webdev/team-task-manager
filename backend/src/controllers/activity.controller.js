41bc178ab9dba38f4f76357cce2f2d00bb30094d
Rehan Raja <rehanraja.dev@gmail.com>
2026-08-01 01:43:03 +0530

    Add caching mechanism to getAllActivities for improved performance

--- backend/src/controllers/activity.controller.js
+++ backend/src/controllers/activity.controller.js
@@ -2,6 +2,7 @@ import Activity from "../models/activity.model.js";
 import ApiError from "../utils/ApiError.js";
 import ApiResponse from "../utils/ApiResponse.js";
 import asyncHandler from "../utils/asyncHandler.js";
+import cacheHelper from "../utils/cache.helper.js";
 
 const getTaskActivities = asyncHandler(async (req, res) => {
   const activities = await Activity.find({ task: req.params.taskId })
@@ -18,12 +19,32 @@ const getAllActivities = asyncHandler(async (req, res) => {
   const limit = Number(req.query.limit) || 20;
   const skip = (page - 1) * limit;
 
+  const cacheKey = `activities_${req.user._id}_${page}_${limit}_${skip}`;
+  const cached = cacheHelper.getCache(cacheKey);
+
+  if (cached && cached.expiresAt > Date.now()) {
+    console.log("Cache Hit:", cacheKey);
+    return res
+      .status(200)
+      .json(new ApiResponse(200, "Activities fetched from cache", cached.data));
+  } else {
+    console.log("Cache Miss:", cacheKey);
+    cacheHelper.deleteCache(cacheKey);
+  }
+
   const activities = await Activity.find()
     .sort({ createdAt: -1 })
     .skip(skip)
     .limit(limit);
 
   const total = await Activity.countDocuments();
+
+  cacheHelper.setCache(cacheKey, {
+    activities,
+    currentPage: page,
+    totalPages: Math.ceil(total / limit),
+    hasMore: skip + activities.length < total,
+  });
 
   res.json({
     activities,

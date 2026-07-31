b130558924bf175b84bd2e4a2f8fe068f50ee2fa
Rehan Raja <rehanraja.dev@gmail.com>
2026-08-01 01:15:48 +0530

    Add ActivityHeader component for displaying activity log title and description

---
+++ frontend/src/components/activity/ActivityHeader.jsx
@@ -0,0 +1,12 @@
+const ActivityHeader = () => {
+  return (
+    <div className="bg-slate-900 p-6 text-center rounded-3xl">
+      <h1 className="text-white font-bold text-4xl">Activity Log</h1>
+      <p className="pt-3 text-slate-400">
+        Track every action happining in your workplace
+      </p>
+    </div>
+  );
+};
+
+export default ActivityHeader;

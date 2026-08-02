69da1b63de76b6f75951b7ca83e7f95db385487e
Rehan Raja <rehanraja.dev@gmail.com>
2026-08-03 04:02:24 +0530

    Add dummy data due to insufficient actual data

--- frontend/src/components/analytics/MonthlyTasksChart.jsx
+++ frontend/src/components/analytics/MonthlyTasksChart.jsx
@@ -9,6 +9,56 @@ import {
 } from "recharts";
 
 const MonthlyTaskChart = ({ data }) => {
+  const dummyyData = [
+    {
+      month: "Jan",
+      tasks: 73,
+    },
+    {
+      month: "Feb",
+      tasks: 153,
+    },
+    {
+      month: "Mar",
+      tasks: 223,
+    },
+    {
+      month: "Apr",
+      tasks: 175,
+    },
+    {
+      month: "May",
+      tasks: 90,
+    },
+    {
+      month: "Jun",
+      tasks: 160,
+    },
+    {
+      month: "Jul",
+      tasks: 139,
+    },
+    {
+      month: "Aug",
+      tasks: 223,
+    },
+    {
+      month: "Sep",
+      tasks: 23,
+    },
+    {
+      month: "Oct",
+      tasks: 133,
+    },
+    {
+      month: "Nov",
+      tasks: 173,
+    },
+    {
+      month: "Dec",
+      tasks: 113,
+    },
+  ];
   return (
     <div className="bg-slate-900 rounded-2xl p-6 border border-slate-800">
       <h2 className="text-xl font-semibold text-white mb-6">
@@ -16,7 +66,7 @@ const MonthlyTaskChart = ({ data }) => {
       </h2>
 
       <ResponsiveContainer width="100%" height={350}>
-        <LineChart data={data}>
+        <LineChart data={dummyyData}>
           <CartesianGrid strokeDasharray="3 3" />
 
           <XAxis dataKey="month" />

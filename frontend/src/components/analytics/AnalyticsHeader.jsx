f03178e01c2f3cf66869ffacb7789a7bb88719e4
Rehan Raja <rehanraja.dev@gmail.com>
2026-08-02 17:50:38 +0530

    Add AnalyticsHeader component and integrate it into Analytics page

---
+++ frontend/src/components/analytics/AnalyticsHeader.jsx
@@ -0,0 +1,7 @@
+const AnalyticsHeader = () => {
+  return (
+    <div>AnalyticsHeader</div>
+  )
+}
+
+export default AnalyticsHeader
---
+++ frontend/src/pages/Analytics.jsx
@@ -0,0 +1,11 @@
+import AnalyticsHeader from "../components/analytics/AnalyticsHeader";
+
+const Analytics = () => {
+  return (
+    <div className="space-y-6">
+      <AnalyticsHeader />
+    </div>
+  );
+};
+
+export default Analytics;

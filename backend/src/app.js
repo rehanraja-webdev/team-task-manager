0bcb04bf4a3cff55c127def1ee8cb6b2170af841
Rehan Raja <rehanraja.dev@gmail.com>
2026-08-17 14:34:57 +0530

    Update CORS configuration to allow localhost for development and remove trailing slash from SITE_URL in SEO component

--- backend/src/app.js
+++ backend/src/app.js
@@ -24,7 +24,7 @@ import swaggerSpec from "./config/swagger.js";
 const app = express();
 app.use(
   cors({
-    origin: [process.env.CLIENT_URL],
+    origin: [process.env.CLIENT_URL, "http://localhost:5173"],
     credentials: true,
   }),
 );
